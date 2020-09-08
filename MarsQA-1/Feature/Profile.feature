f7de6186b72c950abf7493c49cf252a1db89899d
Damart <limbo12_@hotmail.com>
2020-09-08 23:14:12 +0100

    First Commit

---
+++ MarsQA-1/Feature/Profile.feature
@@ -0,0 +1,9 @@
+﻿Feature: Profile
+	
+
+@mytag
+Scenario: As a Seller I want to add my profile Details
+	Given the User is in the Profile page
+	And the User has log-In
+	When the User fills the profile details
+	Then the User is able to see the profile details in the profile page
---
+++ MarsQA-1/SpecflowSteps/ProfileSteps.cs
@@ -0,0 +1,34 @@
+﻿using MarsQA_1.Helpers;
+using System;
+using TechTalk.SpecFlow;
+
+namespace MarsQA_1.SpecflowSteps
+{
+    [Binding]
+    public class ProfileSteps
+    {
+        [Given(@"the User is in the Profile page")]
+        public void GivenTheUserIsInTheProfilePage()
+        {
+            
+        }
+
+        [Given(@"the User has log-In")]
+        public void GivenTheUserHasLog_In()
+        {
+            
+        }
+        
+        [When(@"the User fills the profile details")]
+        public void WhenTheUserFillsTheProfileDetails()
+        {
+            
+        }
+        
+        [Then(@"the User is able to see the profile details in the profile page")]
+        public void ThenTheUserIsAbleToSeeTheProfileDetailsInTheProfilePage()
+        {
+            
+        }
+    }
+}
